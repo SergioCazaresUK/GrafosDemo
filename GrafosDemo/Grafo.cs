b67eaade21304b80f17f6fdd2345a61ce0011431
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-06-23 12:52:08 -0700

    Agregar archivos de proyecto.

---
+++ GrafosDemo/Grafo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    public class CantGrafos
+    {
+        int[,] Adyacencia;
+        int[] indegree;
+        int nodo;
+
+        public CantGrafos(int Nodos)
+        {
+            nodo = Nodos;
+            Adyacencia = new int[nodo, nodo];
+
+            indegree = new int[nodo];
+        }
+
+        public void Arista(int NodoInicio, int NodoFinal)
+        {
+            Adyacencia [NodoInicio, NodoFinal] = 1;
+        }
+
+        public void MuestraAdyacencia()
+        {
+            int n = 1;
+            int m = 1;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (n = 1; n < nodo; n++)
+                Console.WriteLine(n);
+
+            Console.WriteLine();
+
+            for(n = 1; n < nodo; n++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(n);
+                for(m = 1; m < nodo; m++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Adyacencia[n, m]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ GrafosDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grafos;
+
+int nodo = 0;
+CantGrafos grafos = new CantGrafos(6);
+
+grafos.Arista(1, 2);
+grafos.Arista(1, 3);
+grafos.Arista(2, 4);
+grafos.Arista(3, 5);
+grafos.Arista(4, 3);
+grafos.Arista(5, 1);
+grafos.Arista(5, 4);
+
+grafos.MuestraAdyacencia();
